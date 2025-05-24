02ee449e081ffb571baabbbeb111963524b2e805
Jinacio23 <jinaciocemar@gmail.com>
2025-05-24 17:21:23 -0300

    config CORS

--- src/main/java/ticketEstacionamento/controller/TicketController.java
+++ src/main/java/ticketEstacionamento/controller/TicketController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/ticket")
-//@CrossOrigin(origins = "http://localhost:5173")
 public class TicketController {
 
     @Autowired
--- src/main/java/ticketEstacionamento/controller/TokenController.java
+++ src/main/java/ticketEstacionamento/controller/TokenController.java
@@ -19,7 +19,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
-//@CrossOrigin(origins = "http://localhost:5173")
 public class TokenController {
 
     @Autowired
